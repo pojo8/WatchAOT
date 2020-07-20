38c5fb245b92021fc107406e06ef4ab20123d81f
kopis8 <paul-oj@hotmail.co.uk>
2020-07-20 19:04:46 +0100

    adding src

---
+++ aot-anime/src/main/java/venous/data/aotanime/AotAnimeApplication.java
@@ -0,0 +1,13 @@
+package venous.data.aotanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AotAnimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AotAnimeApplication.class, args);
+    }
+
+}
---
+++ aot-anime/src/test/java/venous/data/aotanime/AotAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package venous.data.aotanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AotAnimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
