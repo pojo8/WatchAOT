2ac53d53397cb69bfefdc52ccef8013567fe5e03
kopis8 <paul-oj@hotmail.co.uk>
2020-08-04 17:39:35 +0100

    making season id serial

--- sql/create season listing.sql
+++ sql/create season listing.sql
@@ -4,7 +4,7 @@ create table season_listings
     aired       date                                                               not null,
     description text,
     viewed      integer default 0,
-    season_id integer not null
+    season_id serial not null
         constraint season_listings_pk
             primary key,
     thumbnail   varchar(120)
