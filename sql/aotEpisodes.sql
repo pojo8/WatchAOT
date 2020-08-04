15450e4e9f4d80bd300e8f2ea5664772e28ac85e
Venousdata <37413458+Venousdata@users.noreply.github.com>
2020-08-04 18:00:25 +0100

    formatted queries

--- sql/aotEpisodes.sql
+++ sql/aotEpisodes.sql
@@ -1,19 +1,18 @@
 create table aot_episodes
 (
-    episode_id serial  not null
+    episode_id serial not null
         constraint aot_episodes_pk
             primary key,
-    title      varchar(60),
-    season_id  integer not null
+    title varchar(60),
+    season_id integer not null
         constraint season_id_fk
             references season_listings,
-    source1    varchar(120),
-    source2    varchar(120),
-    source3    varchar(120),
-    source4    varchar(120),
-    source5    varchar(120),
-    source6    varchar(120),
-    views      integer default 0,
-    aired      date,
-
+    source1 varchar(120),
+    source2 varchar(120),
+    source3 varchar(120),
+    source4 varchar(120),
+    source5 varchar(120),
+    source6 varchar(120),
+    views integer default 0,
+    aired date
 );
