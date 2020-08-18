3e00878efaccb7b442d885e46a06714d82e14c88
Venousdata <37413458+Venousdata@users.noreply.github.com>
2020-08-18 18:51:04 +0100

    added sort label to episodes table UNFINISHED

--- frontend/src/components/EpisodeList.jsx
+++ frontend/src/components/EpisodeList.jsx
@@ -1,28 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Button,
+} from "@material-ui/core/";
 
 const useStyles = makeStyles((theme) => {
-  return {};
+  return {
+    visuallyHidden: {
+      border: 0,
+      clip: "rect(0 0 0 0)",
+      height: 1,
+      margin: -1,
+      overflow: "hidden",
+      padding: 0,
+      position: "absolute",
+      top: 20,
+      width: 1,
+    },
+  };
 });
 
 function EpisodeList({ episodes }) {
   console.log("==========episodes==========", episodes);
   const classes = useStyles();
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("number");
   const rows = [];
 
-  function createData(title, season, views, watchButton) {
-    return { title, season, views, watchButton };
+  function createData(number, title, season, views, watchButton) {
+    return { number, title, season, views, watchButton };
   }
+
+  function clicked(e) {
+    console.log("==========e==========", e);
+  }
+
+  const onRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    console.log("==========order==========", order);
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const createSortHandler = (property) => (event) => {
+    // console.log("==========headcellID==========", property);
+    onRequestSort(event, property);
+  };
+
+  const headCells = [
+    {
+      id: "number",
+      numeric: true,
+      disablePadding: true,
+      label: "Episode number",
+    },
+    { id: "title", numeric: false, disablePadding: false, label: "Title" },
+    { id: "season", numeric: true, disablePadding: false, label: "Season" },
+    { id: "views", numeric: true, disablePadding: false, label: "Views" },
+  ];
 
   episodes.forEach((episode) =>
     rows.push(
-      createData(episode.title, episode.season_Id, episode.views, "button")
+      createData(
+        episode.episode_Number,
+        episode.episode_Title,
+        episode.season_Id,
+        episode.views
+      )
     )
   );
 
@@ -33,21 +84,40 @@ function EpisodeList({ episodes }) {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Episode Title</TableCell>
-            <TableCell align="right">Season</TableCell>
-            <TableCell align="right">Views</TableCell>
-            <TableCell align="right">Watch</TableCell>
+            {headCells.map((headCell) => (
+              <TableCell
+                key={headCell.id}
+                align={headCell.numeric ? "right" : "left"}
+                padding={headCell.disablePadding ? "none" : "default"}
+                sortDirection={orderBy === headCell.id ? order : false}
+              >
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : "asc"}
+                  onClick={createSortHandler(headCell.id)}
+                >
+                  {headCell.label}
+                  {orderBy === headCell.id ? (
+                    <span className={classes.visuallyHidden}>
+                      {order === "desc"
+                        ? "sorted descending"
+                        : "sorted ascending"}
+                    </span>
+                  ) : null}
+                </TableSortLabel>
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row, index) => (
             <TableRow hover key={index}>
-              <TableCell component="th" scope="row">
+              <TableCell>{row.number}</TableCell>
+              <TableCell onClick={clicked} component="th" scope="row">
                 {row.title}
               </TableCell>
               <TableCell align="right">{row.season}</TableCell>
               <TableCell align="right">{row.views}</TableCell>
-              <TableCell align="right">{row.watchButton}</TableCell>
             </TableRow>
           ))}
         </TableBody>
