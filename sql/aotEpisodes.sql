7ae2f5c540daf8d7c8efd134e7ef86cc15e5a8fa
kopis8 <paul-oj@hotmail.co.uk>
2020-08-18 17:39:05 +0100

    updated sql

--- sql/aotEpisodes.sql
+++ sql/aotEpisodes.sql
@@ -4,16 +4,16 @@ create table aot_episodes
         constraint aot_episodes_pk
             primary key,
     episode_title  varchar(80),
+    episode_number varchar(5),
     season_id integer not null
         constraint season_id_fk
             references season_listings,
-    episode_number varchar(5),
     source1 varchar(120),
     source2 varchar(120),
     source3 varchar(120),
     source4 varchar(120),
     source5 varchar(120),
     source6 varchar(120),
+    aired date,
     views integer default 0,
-    aired date
 );
