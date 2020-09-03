a7014b45b18a5d9ee3cd15b8f85e7a34fc5bdb6b
Venousdata <37413458+Venousdata@users.noreply.github.com>
2020-09-03 18:18:47 +0100

    testing modal, unfinished

--- frontend/src/components/SeasonInfo.jsx
+++ frontend/src/components/SeasonInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, Redirect, Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import {
@@ -8,7 +8,7 @@ import {
   Typography,
   CardContent,
   CardActionArea,
-  ButtonBase,
+  Modal,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => {
@@ -28,20 +28,38 @@ const useStyles = makeStyles((theme) => {
 });
 
 function SeasonsInfo({ season, index }) {
+  const [openModal, setOpenModal] = useState(false);
+
   const history = useHistory();
   const classes = useStyles();
+
+  const handleClick = (event) => {
+    setOpenModal(true);
+    console.log("==========event==========", event);
+  };
+
   return (
-    <Grid className={classes.grid} key={index} item>
-      <Card>
-        <CardActionArea>
-          <CardMedia className={classes.cardmedia} image={season.thumbnail} />
-          <CardContent className={classes.cardcontent}>
-            <Typography variant="h5">{season.title}</Typography>
-            <Typography variant="body2">{season.description}</Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
+    <>
+      <Modal open={openModal}>
+        <div>
+          <h2 id="simple-modal-title">Text in a modal</h2>
+          <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+        </div>
+      </Modal>
+      <Grid className={classes.grid} key={index} item>
+        <Card>
+          <CardActionArea onClick={handleClick}>
+            <CardMedia className={classes.cardmedia} image={season.thumbnail} />
+            <CardContent className={classes.cardcontent}>
+              <Typography variant="h5">{season.title}</Typography>
+              <Typography variant="body2">{season.description}</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </>
   );
 }
 
