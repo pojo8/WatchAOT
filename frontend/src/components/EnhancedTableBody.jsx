39cfca8a38d540ad749813494a2cf23505bc3115
Venousdata <37413458+Venousdata@users.noreply.github.com>
2020-10-14 18:08:18 +0100

    new video player component open on episode click

--- frontend/src/components/EnhancedTableBody.jsx
+++ frontend/src/components/EnhancedTableBody.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
+import CustomDialog from "./CustomDialog";
+import VideoPlayer from "./VideoPlayer";
 
 import { TableCell, TableRow, TableBody } from "@material-ui/core";
 
@@ -10,22 +12,40 @@ function EnhancedTableBody({
   stableSort,
   getComparator,
 }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleDialogOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleDialogClose = () => setIsOpen(false);
+
   return (
-    <TableBody>
-      {stableSort(rows, getComparator(order, orderBy)).map((row) => {
-        return (
-          <TableRow hover tabIndex={-1} key={row.number}>
-            {Object.keys(row).map((property) => (
-              <TableCell
-                align={typeof row[property] === "number" ? "right" : "none"}
-              >
-                {row[property]}
-              </TableCell>
-            ))}
-          </TableRow>
-        );
-      })}
-    </TableBody>
+    <>
+      <CustomDialog
+        isOpen={isOpen}
+        handleClose={handleDialogClose}
+        title={`Episode TEST`}
+      >
+        <VideoPlayer />
+      </CustomDialog>
+      <TableBody>
+        {stableSort(rows, getComparator(order, orderBy)).map((row) => {
+          return (
+            <TableRow hover tabIndex={-1} key={row.number}>
+              {Object.keys(row).map((property) => (
+                <TableCell
+                  align={typeof row[property] === "number" ? "right" : "none"}
+                  onClick={handleDialogOpen}
+                >
+                  {row[property]}
+                </TableCell>
+              ))}
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </>
   );
 }
 
---
+++ frontend/src/components/VideoPlayer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles((theme) => {
+  return {};
+});
+
+function VideoPlayer() {
+  const classes = useStyles();
+
+  return (
+    <div>HI YOU'RE HERE</div>
+  );
+}
+
+export default VideoPlayer;
+
+VideoPlayer.propTypes = {
+};
