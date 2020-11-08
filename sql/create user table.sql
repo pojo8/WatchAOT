db692c9b703fb495c542d90cf1a0260166c05ab2
Paul Ojomo <35232131+pojo8@users.noreply.github.com>
2020-11-08 23:15:42 +0000

    Update create user table.sql

--- sql/create user table.sql
+++ sql/create user table.sql
@@ -3,9 +3,12 @@ create table users
     user_id          bigserial   not null
         constraint users_pk
             primary key,
-    email            varchar(40) not null,
+    email            varchar(80) not null
+        constraint users_pk_2
+            unique,
     username         varchar(25) not null,
     password_hash    varchar     not null,
     pwd_reset_expiry timestamp with time zone,
-    reset_pwd        boolean default false
+    reset_pwd        boolean default false,
+    password_expiry  timestamp
 );
